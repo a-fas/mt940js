4992c8bd8af943db4e6fd9e2c2d5553739444b9e
a-fas <a-fas@users.noreply.github.com>
2016-11-29 13:30:09 +0200

    Tag parsing

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const MT940Parser = require('./lib/parser');
 
 const data = fs.readFileSync('./tests/test1.txt', { encoding: 'utf8' });
 
+
 const parser = new MT940Parser();
 const tags = parser.parse(data);
 
 for (let i of tags) {
-  console.log(i);
+  console.log(i.id, i.data, i.fields);
 }
 
+//*******************************************************
+// Playground
+//*******************************************************
 
-// const MT940TagFactory = require('./lib/tags');
-// const factory = new MT940TagFactory.TagFactory();
+
+// const TagFactory  = require('./lib/tags').TagFactory;
+// const tf  = new TagFactory();
+// const tag = tf.createTag(20, '', 'OLOLO');
+//
+// console.log(tag);
+// console.log(tag.fields);
+
+
+// tag
