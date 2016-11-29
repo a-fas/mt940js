1daf55a8412319156201b825829edaf4f78bcbed
a-fas <a-fas@users.noreply.github.com>
2016-11-29 16:50:25 +0200

    building statements

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ const data = fs.readFileSync('./tests/test1.txt', { encoding: 'utf8' });
 
 
 const parser = new MT940Parser();
-const tags = parser.parse(data);
-
-for (let i of tags) {
-  console.log(i.id, i.data, i.fields);
-}
-
-//*******************************************************
-// Playground
-//*******************************************************
 
 
-// const TagFactory  = require('./lib/tags').TagFactory;
-// const tf  = new TagFactory();
-// const tag = tf.createTag(20, '', 'OLOLO');
-//
-// console.log(tag);
-// console.log(tag.fields);
+const statements = parser.parse(data, true);
 
-
-// tag
+statements.forEach(st => {
+  console.log('');
+  console.log(st);
+});
